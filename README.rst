9657b57eb8ebc62c5a370f7b08b0a64c604ffb35
Stephane Raynaud <stephane.raynaud@shom.fr>
2023-10-18 17:43:01 +0200

    Add basic files

---
+++ README.rst
@@ -0,0 +1,4 @@
+woom - workflow manager for ocean models
+========================================
+
+woom is designed to help you submit workflows for running ocean models.
--- woom/__init__.py
+++ woom/__init__.py
@@ -3,6 +3,15 @@
 """
 Light weight workflow manager for ocean models
 """
+from importlib.metadata import version as _version
+
+
+try:
+    __version__ = _version("xarray")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "9999"
 
 
 class WoomError(Exception):
